a8e90ebc312c70f4a5d276d8ea27a5c6dd810332
Sachin Choudhary <sachinchoudhary2129@gmail.com>
2025-08-18 10:43:45 +0530

    01-IMS update db

--- includes/db.php
+++ includes/db.php
@@ -2,8 +2,8 @@
 $host = "localhost";
 $port = 3306; // or 3306
 $user = "root";
-$pass = "";
-$dbname = "indsac_internship";
+$pass = "mysql";
+$dbname = "internship_db";
 $uploadFolder ="upload";
 
 try {
